f7b568fa10e4013fdaf5ceda3e865897979e8884
Ritamking <115737287+Ritamking@users.noreply.github.com>
2023-11-18 01:23:09 +0530

    Update test.py

--- test.py
+++ test.py
@@ -13,7 +13,7 @@ imgSize = 300
 
 counter = 0
 
-labels = ["A", "B", "C" , "D" , "E" , "F" , "G", "H"] 
+labels = ["A", "B", "C" , "D" , "E" , "F" , "G", "H", "I", "J", "K", "L", "M", "N","O" , "P", "Q", "R" , "S", "T", "U", "V", "W", "X", "Y", "Z"] 
 
 while True:
     success, img = cap.read()
